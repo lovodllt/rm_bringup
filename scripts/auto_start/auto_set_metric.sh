55434ad23be08a37f8545437f2549779103ed1cf
王湘鈜 <3328432363@qq.com>
2024-01-13 22:48:03 +0800

    Add script and service about auto set metric to solve the confict between ethercat and switch.

---
+++ scripts/auto_start/auto_set_metric.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Starting auto_set_metric.sh"
+cd /home/dynamicx
+sudo ifmetric enp86s0 200
+echo "enp86s0 Metric: $(route -n | grep '0.0.0.0' | grep 'enp86s0' | awk '{print $5}'
+)"
+sudo ifmetric enx081f71632e17 100
+echo "enx081f71632e17 Metric: $(route -n | grep '0.0.0.0' | grep 'enx081f71632e17' | awk '{print $5}'
+)"
+echo "auto_set_metric.sh executed successfully"
