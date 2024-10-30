e321c736fee833de870c076466be4c1941ea6ac6
chen-gr <725393026@qq.com>
2024-10-30 20:25:44 +0800

    Remove service variables and load restart directly.

--- scripts/auto_start/check_ip.sh
+++ scripts/auto_start/check_ip.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-service_name="rm_ecat_start.service"
 target_ip="192.168.100.2/24"
 restarted=false
 
@@ -8,8 +7,8 @@ while true; do
     if ! ip addr show | grep -q -w "$target_ip"; then
         if [ "$restarted" = false ]; then
             echo "IP $target_ip not found."
-            echo "Restarting services: $service_name, start_master.service, vision_start_service..."
-            sudo systemctl restart "$service_name" start_master.service vision_start.service
+            echo "Restarting services: rm_ecat_start.service, start_master.service, vision_start_service..."
+            sudo systemctl restart rm_ecat_start.service start_master.service vision_start.service
             echo "Services restarted successfully.Exiting."
             restarted=true
             break
