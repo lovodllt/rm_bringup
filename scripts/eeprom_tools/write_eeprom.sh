7b0d5e6e43176457511a97eb4dcbc7f69b0e5292
youjian <1124895509@qq.com>
2025-06-07 11:32:21 +0800

    Add executable permissions for eepromtool.

--- scripts/eeprom_tools/write_eeprom.sh
+++ scripts/eeprom_tools/write_eeprom.sh
@@ -3,22 +3,22 @@
 export ToolPath="/home/dynamicx/rm_ws/src/rm_bringup/scripts/eeprom_tools"
 export EthName="enp49s0"
 export SlaveOrder=1
-export EepromType="MIT"  # use DBUS for rm/dbus slaves, or use MIT for mit slaves
+export EEpromType="MIT"  # use DBUS for rm/dbus slaves, or use MIT for mit slaves
 
 sudo su -c "
     export ToolPath=\"$ToolPath\"
     export EthName=\"$EthName\"
     export SlaveOrder=\"$SlaveOrder\"
-    export EepromType=\"$EepromType\"
+    export EEpromType=\"$EEpromType\"
 
     cd \$ToolPath || { echo \"\$ToolPath does not exist, please check again\"; exit 1; }
-
-    if [ \"\$EepromType\" = \"DBUS\" ]; then
+    chmod 777 eepromtool
+    if [ \"\$EEpromType\" = \"DBUS\" ]; then
         ./eepromtool \$EthName \$SlaveOrder -wi eeprom_dbus.hex
-    elif [ \"\$EepromType\" = \"MIT\" ]; then
+    elif [ \"\$EEpromType\" = \"MIT\" ]; then
         ./eepromtool \$EthName \$SlaveOrder -wi eeprom_mit.hex
     else
-        echo \"Unknown EepromType: \$EepromType\"
+        echo \"Unknown EEpromType: \EEpromType\"
         exit 1
     fi
     exit
